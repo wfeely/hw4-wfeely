a1b6ba3d83ed9bb4c3ebdaa1dd219abaadda5b42
wfeely <wfeely@cs.cmu.edu>
2013-10-14 13:44:50 -0400

    updated with better iteration of FSList

--- hw4-wfeely/src/main/java/edu/cmu/lti/f13/hw4/hw4_wfeely/annotators/DocumentVectorAnnotator.java
+++ hw4-wfeely/src/main/java/edu/cmu/lti/f13/hw4/hw4_wfeely/annotators/DocumentVectorAnnotator.java
@@ -3,22 +3,19 @@
  */
 package edu.cmu.lti.f13.hw4.hw4_wfeely.annotators;
 
-import java.util.Iterator;
+import java.util.ArrayList;
 
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.cas.FSIndex;
 import org.apache.uima.cas.FSIterator;
-import org.apache.uima.cas.FeatureStructure;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSList;
-import org.apache.uima.jcas.cas.IntegerArray;
 import org.apache.uima.jcas.cas.NonEmptyFSList;
-import org.apache.uima.jcas.cas.StringArray;
 import org.apache.uima.jcas.tcas.Annotation;
 
 import edu.cmu.lti.f13.hw4.hw4_wfeely.typesystems.Document;
 import edu.cmu.lti.f13.hw4.hw4_wfeely.typesystems.Token;
+
 
 public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 
@@ -42,31 +39,51 @@ public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
   private void createTermFreqVector(JCas jcas, Document doc) {
 
     String docText = doc.getText();
-    // TODO: construct a vector of tokens and update the tokenList in CAS
+    // construct a vector of tokens and update the tokenList in CAS
+    // copy list elements into an arrayList
     FSList list = doc.getTokenList();
-    for (String tokenText : docText.split(" ")) {
-      // set up token
-      Token tok = null;
-      tok.setText(tokenText);
-      tok.setFrequency(1);
-      // loop through tokens in tokenList
-      boolean match = false;
-      while (doc.getTokenList() instanceof NonEmptyFSList) {
-        Token head = (Token) ((NonEmptyFSList) list).getHead();
-        // compare each token in tokenList to tok
-        if (head.getText() == tok.getText()) {
-          // found a match; increment the frequency of this token, and break loop
-          int freq = head.getFrequency() + 1;
-          head.setFrequency(freq);
-          match = true;
-          list = ((NonEmptyFSList) list).getTail();
-          break;
-        }
-        list = ((NonEmptyFSList) list).getTail();
+    ArrayList<Token> aList = new ArrayList<Token>();
+    Token listTok = null;
+    int i = 0;
+    while (true) {
+      try {
+        listTok = (Token) list.getNthElement(i);
+      } catch (Exception e) {
+        // no more elements to process
+        break;
       }
-      if (!match) {
-        // TODO: add token to FSList
-      }
+      aList.add(listTok);
+      i++;
     }
+    // loop through token strings in document
+    for (String tokenText : docText.split(" ")) {
+      // set up new token
+      Token docTok = new Token(jcas);
+      docTok.setText(tokenText);
+      docTok.setFrequency(1);
+      // loop through tokens in aList
+      boolean match = false;
+      for (Token aListTok : aList) {
+        // compare document token with current token from arrayList aList
+        if (docTok.getText() == aListTok.getText()) {
+          // match; increment frequency of this type and set match to true
+          aListTok.setFrequency(aListTok.getFrequency() + 1);
+          match = true;
+        }
+      }
+      // no match; add this new type to aList
+      if (!match)
+        aList.add(docTok);
+    }
+    // reset FSList list, by copying arrayList aList back into it
+    FSList outList = new FSList(jcas);
+    ((NonEmptyFSList) outList).setHead(aList.get(0));
+    ((NonEmptyFSList) outList).setTail(null);
+    for (int j=1; j < aList.size(); j++) {
+      Token currentToken = aList.get(j);
+      outList = ((NonEmptyFSList) outList).getTail();
+      ((NonEmptyFSList) outList).setHead(currentToken);
+    }
+    doc.setTokenList(outList);
   }
 }
